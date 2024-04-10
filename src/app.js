266a3cf77e6b1eb5f65dfe0dafefcd4b56ebf139
daniel <dg189948@gmail.com>
2024-04-10 10:53:59 -0300

    aula 2

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import express from "express"
 
 export const App = express();
+App.use(express.json())
+const livros = [
+    {id: 1, nome: "O hobbit"},
+    {id: 2, nome: "O senhor dos anéis"},
+]
 
 App.get("/", (req,res)=>{
-    res.status(200).send("hello world")
+    res.status(200).json(livros)
+})
+
+App.post("/", (req,res)=>{
+    livros.push(req.body)
+    res.status(201).send("livro inserido com sucesso")
 })
---
+++ src/dbconnect.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export const connect = async () => {
+    mongoose.connect("mongodb+srv://root:root@cluster0.cxbbh8f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    
+}
